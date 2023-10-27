60e34f23de14752df5eeded22a6649ab3f001837
chris <chris@bumblehead.com>
2023-10-26 23:49:07 -0700

    convert to import

--- test/domwh.spec.js
+++
@@ -1,7 +0,0 @@
-var domwh = require('../domwh');
-
-describe("domwh", function () {
-  it("should not break server side script environment", function () {  
-    expect( typeof domwh ).toBe( 'function' );
-  });
-});
